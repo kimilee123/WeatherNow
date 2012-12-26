08eab29cf59e06dbeddfb7e206b47346de7368c2
kk <kk@kktekiMac-mini.local>
2012-12-26 17:04:51 +0800

    Initial Commit

---
+++ KTdemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KTdemo
+//
+//  Created by kk on 12-12-26.
+//  Copyright (c) 2012年 kk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KTdemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KTdemo
+//
+//  Created by kk on 12-12-26.
+//  Copyright (c) 2012年 kk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
